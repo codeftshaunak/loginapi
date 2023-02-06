6a762bfcf942cbe0e47e84df254884bc1ac543d3
Shaunak KD <shaunakkd@Shaunaks-MacBook-Air.local>
2023-02-06 22:49:16 +0600

    api intigration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    const sendData = async () => {
+      try {
+        const data = { session_id: 'un6beoydda13mcno4kv8lsady27motnz' };
+        const response = await axios.post('https://100014.pythonanywhere.com/api/userinfo/', data);
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    sendData();
+  }, []);
+
+  useEffect(() => {
+    const sendData = async () => {
+      try {
+        const data = { session_id: 'un6beoydda13mcno4kv8lsady27motnz', id:'100098' };
+        const response = await axios.post('https://100093.pythonanywhere.com/api/userinfo/', data);
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    sendData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="button">
+      <button>
+        <a href="https://100014.pythonanywhere.com/">LogIn</a>
+      </button>
+     </div>
     </div>
   );
 }
